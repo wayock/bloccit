8618028faa040c0f0484c1629645d1b2c009936e
Matthew Wayock <wayock@gmail.com>
2019-06-08 15:09:15 -0400

    express intro assignment completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -18,6 +19,25 @@ describe("routes : static", () => {
         done();
       });
     });
+
+  });
+
+  describe("GET /marco", () => {
+
+    it("should return status code 200", (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("should return string 'polo'", done => {
+      request.get(marco, (err, res, body) => {
+        expect(body).toContain('polo');
+        done();
+    });
+
+  });
 
   });
 });
