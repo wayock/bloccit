d251041e2eb0ed86e7a7e7d54eb3ac166237e826
Matthew Wayock <wayock@gmail.com>
2019-06-14 06:05:40 -0400

    trying to run migrations to create banners table, error: ERROR: relation "Topic" does not exist

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -6,6 +6,10 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Topic.associate = function(models) {
     // associations can be defined here
+    Topic.hasMany(models.Banner, {
+     foreignKey: "topicId",
+     as: "banners",
+   });
   };
   return Topic;
 };
