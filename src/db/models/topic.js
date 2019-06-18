dffac4ec02d52e827f6a29352de0c6ad51a326dc
Matthew Wayock <wayock@gmail.com>
2019-06-18 08:48:30 -0400

    completed post resource part 1 ckpt, testing & associating post and topic models

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,15 +1,25 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    title: {
+       type: DataTypes.STRING,
+       allowNull: false
+     },
+    description: {
+       type: DataTypes.STRING,
+       allowNull: false
+    }
   }, {});
   Topic.associate = function(models) {
     // associations can be defined here
     Topic.hasMany(models.Banner, {
-     foreignKey: "topicId",
-     as: "banners",
-   });
+      foreignKey: "topicId",
+      as: "banners",
+    });
+    Topic.hasMany(models.Post, {
+      foreignKey: "topicId",
+      as: "posts"
+    });
   };
   return Topic;
 };
