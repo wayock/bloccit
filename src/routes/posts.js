2f951bf5f6d13804b5132e9f540eaf590a8f6d52
Matthew Wayock <wayock@gmail.com>
2019-06-19 13:24:27 -0400

    checkpoint completed: implemented the CRUD actions for the Post resource

---
+++ src/routes/posts.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const postController = require("../controllers/postController")
+
+router.get("/topics/:topicId/posts/new", postController.new);
+router.get("/topics/:topicId/posts/:id", postController.show);
+router.get("/topics/:topicId/posts/:id/edit", postController.edit);
+router.post("/topics/:topicId/posts/create", postController.create);
+router.post("/topics/:topicId/posts/:id/destroy", postController.destroy);
+router.post("/topics/:topicId/posts/:id/update", postController.update);
+
+
+module.exports = router;
