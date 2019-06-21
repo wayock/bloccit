a2ce37bfc5ec8d69bfb6f47878dc5f704bc64a8e
Matthew Wayock <wayock@gmail.com>
2019-06-21 17:15:43 -0400

    post validations added - checkpoint completed

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -81,6 +81,34 @@ describe("routes : posts", () => {
       );
      });
 
+     it("should not create a new post that fails validations", (done) => {
+            const options = {
+              url: `${base}/${this.topic.id}/posts/create`,
+              form: {
+
+     //#1
+                title: "a",
+                body: "b"
+              }
+            };
+
+            request.post(options,
+              (err, res, body) => {
+
+     //#2
+                Post.findOne({where: {title: "a"}})
+                .then((post) => {
+                    expect(post).toBeNull();
+                    done();
+                })
+                .catch((err) => {
+                  console.log(err);
+                  done();
+                });
+              }
+            );
+          });
+
    });
 
    describe("GET /topics/:topicId/posts/:id", () => {
@@ -147,7 +175,8 @@ describe("routes : posts", () => {
              const options = {
                url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
                form: {
-                 title: "Snowman Building Competition"
+                 title: "Snowman Building Competition",
+                 body: "I really enjoy the funny hats on them."
                }
              };
              request.post(options,
